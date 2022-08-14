28b271ef752a7019f6063f653a8f2160c8a9768d
Shourov Hasan <helloshourov2you@gmail.com>
2022-08-14 20:21:43 +0600

    All pages done with phone responsive

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
     
     var one = $("#testimonial_carousel");
     var two = $("#product_carousel");
+    var three = $("#article_slider");
+    var four = $("#product_cover_slider");
 
 // testimonial_carousel_start
 one.owlCarousel({
@@ -58,6 +60,57 @@ two.owlCarousel({
 
 // product_carousel_end
 
+// article_carousel_start
+
+three.owlCarousel({
+    loop:true,
+    autoplay: true,
+    autoPlaySpeed: 2000,
+    autoPlayTimeout:1000,
+    autoplayHoverPause: false,
+    dots: false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+// article_carousel_end
+
+// product_page_slider
+four.owlCarousel({
+    loop:true,
+    autoplay: true,
+    autoPlaySpeed: 2000,
+    autoPlayTimeout:1000,
+    autoplayHoverPause: false,
+    dots: false,
+    nav:true,
+    responsive:{
+        0:{
+            dots:true,
+            nav:false,
+            items:1,
+        },
+        600:{
+            dots:true,
+            nav:false,
+            items:1,
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+// product_page_slider
 
 
 
